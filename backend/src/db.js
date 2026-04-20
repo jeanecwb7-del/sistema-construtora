615f9cfbc63a4307d469cd554e2f25a87d300764
jeanecwb7-del <jeanecwb7@gmail.com>
2026-04-20 10:30:33 -0300

    Create db.js

---
+++ backend/src/db.js
@@ -0,0 +1,11 @@
+const sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database('./database.sqlite', (err) => {
+  if (err) {
+    console.error('Erro ao conectar ao banco:', err.message);
+  } else {
+    console.log('Banco conectado ✅');
+  }
+});
+
+module.exports = db;
