613626f31f5f1d507a97ce8702debf3f680cb4f4
jeanecwb7-del <jeanecwb7@gmail.com>
2026-04-20 10:45:46 -0300

    Update db.js

--- backend/src/database/db.js
+++ backend/src/database/db.js
@@ -1,1 +1,14 @@
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
 
+const dbPath = path.join(__dirname, 'database.sqlite');
+
+const db = new sqlite3.Database(dbPath, (err) => {
+  if (err) {
+    console.error('Erro ao conectar ao banco:', err.message);
+  } else {
+    console.log('Banco conectado ✅');
+  }
+});
+
+module.exports = db;
